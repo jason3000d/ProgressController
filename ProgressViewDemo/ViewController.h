edbc98cf2d26792f434ae6d7efcffbbcf1a0176e
Seraph J. Lin <jason3000d@gmail.com>
2015-06-20 15:35:49 +0800

    Initial Commit

---
+++ ProgressViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProgressViewDemo
+//
+//  Created by Seraph on 2015/6/20.
+//  Copyright (c) 2015年 Seraph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ProgressViewDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ProgressViewDemo
+//
+//  Created by Seraph on 2015/6/20.
+//  Copyright (c) 2015年 Seraph. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
